0f1c7cfb337f0cc086aa1a00323cb477914e9b07
Alvar Aronija <alvar@weappdevelopers.com>
2020-05-16 13:28:34 +0200

    Fetch and show data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import ImageCard from './ImageCard';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect( () => {
+    async function fetchData() {
+      setUsers(
+        await fetch('https://reqres.in/api/users')
+        .then(res => res.json())
+        .then(res => res.data)
+      )
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>
+        RESPONSIVE REACT
+      </h3>
+        <Grid container spacing={10} style={{padding: '24px'}}>
+          {users.map(user => (
+            <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+              <ImageCard 
+                key={user.id}
+                email={user.email}
+                firstName={user.first_name}
+                lastName={user.last_name}
+                avatar={user.avatar}
+              />
+            </Grid>
+          ))}
+        </Grid>
     </div>
   );
 }
